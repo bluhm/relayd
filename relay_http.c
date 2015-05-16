c34fd4f96ca3e9fee145454ea7ca19a296eb73ac
Alexander Bluhm <alexander.bluhm@gmx.net>
2015-05-16 15:17:54 +0200

    Reset http lastheader at the method line to avoid a crash.

--- relay_http.c
+++ relay_http.c
@@ -340,6 +340,7 @@ relay_read_http(struct bufferevent *bev, void *arg)
 		    strcasecmp("chunked", value) == 0)
 			desc->http_chunked = 1;
 
+		desc->http_lastheader = NULL;
 		if (cre->line != 1) {
 			if ((hdr = kv_add(&desc->http_headers, key,
 			    value)) == NULL) {
