14e7ebaa0f35c5be68592b5449c3af589f130df0
Alexander Bluhm <alexander.bluhm@gmx.net>
2015-05-18 18:46:00 +0200

    cvs commit

--- relay_http.c
+++ relay_http.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: relay_http.c,v 1.44 2015/04/29 08:41:24 bluhm Exp $	*/
+/*	$OpenBSD: relay_http.c,v 1.45 2015/05/18 16:45:16 bluhm Exp $	*/
 
 /*
  * Copyright (c) 2006 - 2015 Reyk Floeter <reyk@openbsd.org>
